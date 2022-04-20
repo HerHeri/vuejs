169641592373689cffb814ca28cae258fead9b26
HerHeri <madeheri14@gmail.com>
2022-04-20 11:21:34 +0700

    Update

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
+    <!-- <v-container fluid> -->
+      <input type="text" v-model="text">
+      <h3>Nama : {{  text  }}</h3>
+    <!-- </v-container> -->
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'HomeTest',
+    data() {
+      return{
+        text: "Im Heri"
+      }
+    },
+  }
+</script>
